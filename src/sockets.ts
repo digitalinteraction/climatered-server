592bcd22fb1627f2495b7e70eb37de61a4d201c7
Rob Anderson <rob@andrsn.uk>
2020-06-30 19:48:05 +0100

    prototyping
    - add socket events to join/leave/start/broadcast to channels

--- src/sockets.ts
+++ src/sockets.ts
@@ -11,9 +11,9 @@ import registrations = require('./data/registrations.json')
 const debug = createDebug('api:sockets')
 
 // Send an error back to the socket client
-function sendError(socket: Socket, message: string) {
+function sendError(socket: Socket, from: string, message: string) {
   debug(`sendError id="${socket.id}" message="${message}"`)
-  socket.emit('user-error', { message })
+  socket.emit('user-error', { from, message })
 }
 
 // Get a user registration from a socket.id and redis instance
@@ -46,7 +46,7 @@ export function createSockets(io: SocketServer, redis: Redis, env: Env) {
       if (hasAuth) {
         await redis.set('auth_' + socket.id, token)
       } else {
-        sendError(socket, 'Not authorized')
+        sendError(socket, 'auth', 'Not authorized')
       }
     })
 
@@ -57,10 +57,10 @@ export function createSockets(io: SocketServer, redis: Redis, env: Env) {
       debug(`join-event id=${eventId}`)
 
       const user = await getUser(socket.id, redis)
-      if (!user) return sendError(socket, 'bad_auth')
+      if (!user) return sendError(socket, 'join-event', 'bad_auth')
 
       const event = events.find((e) => e.id === eventId)
-      if (!event) return sendError(socket, `event.not_found`)
+      if (!event) return sendError(socket, 'join-event', `event.not_found`)
       socket.join(event.id)
 
       io.in(event.id).emit('user-joined', { name: user.name })
@@ -73,10 +73,10 @@ export function createSockets(io: SocketServer, redis: Redis, env: Env) {
       debug(`leave-event id=${eventId}`)
 
       const user = await getUser(socket.id, redis)
-      if (!user) return sendError(socket, 'bad_auth')
+      if (!user) return sendError(socket, 'leave-event', 'bad_auth')
 
       const event = events.find((e) => e.id === eventId)
-      if (!event) return sendError(socket, `event.not_found`)
+      if (!event) return sendError(socket, 'leave-event', `event.not_found`)
       socket.leave(event.id)
 
       io.in(event.id).emit('user-left', { name: user.name })
@@ -89,12 +89,122 @@ export function createSockets(io: SocketServer, redis: Redis, env: Env) {
       debug(`chat event=${eventId} message=${message}`)
 
       const user = await getUser(socket.id, redis)
-      if (!user) return sendError(socket, 'bad_auth')
+      if (!user) return sendError(socket, 'chat', 'bad_auth')
 
       const event = events.find((e) => e.id === eventId)
-      if (!event) return sendError(socket, `event.not_found`)
+      if (!event) return sendError(socket, 'chat', `event.not_found`)
 
       io.in(event.id).emit('chat', { name: user.name, message })
+    })
+
+    //
+    // [A prototype refactoring for the below methods]
+    //
+    // async function validateChannel(
+    //   socket: Socket,
+    //   eventId: string,
+    //   channel: string
+    // ) {
+    //   const user = await getUser(socket.id, redis)
+    //   if (!user) throw new Error('bad_auth')
+
+    //   const event = events.find((e) => e.id === eventId)
+    //   if (!event) throw new Error('event_not_found')
+
+    //   const found = event.channels?.includes(channel)
+    //   if (!found) throw new Error('channel_not_found')
+
+    //   return { user, event }
+    // }
+
+    //
+    // Listen to a channel on an event
+    //
+    socket.on('join-channel', async ({ eventId = '', channel = '' }) => {
+      debug(`join-channel event=${eventId} channel=${channel}`)
+
+      const user = await getUser(socket.id, redis)
+      if (!user) return sendError(socket, 'join-channel', 'bad_auth')
+
+      const event = events.find((e) => e.id === eventId)
+      if (!event) return sendError(socket, 'join-channel', `event.not_found`)
+
+      const found = event.channels?.includes(channel)
+      if (!found) return sendError(socket, 'join-channel', `channel.not_found`)
+
+      socket.join(`channel-${event.id}-${channel}`)
+    })
+
+    //
+    // Stop listening to a channel
+    //
+    socket.on('leave-channel', async ({ eventId = '', channel = '' }) => {
+      debug(`leave-channel event=${eventId} channel=${channel}`)
+
+      const user = await getUser(socket.id, redis)
+      if (!user) return sendError(socket, 'leave-channel', 'bad_auth')
+
+      const event = events.find((e) => e.id === eventId)
+      if (!event) return sendError(socket, 'leave-channel', `event.not_found`)
+
+      const found = event.channels?.includes(channel)
+      if (!found) return sendError(socket, 'leave-channel', `channel.not_found`)
+
+      socket.leave(`channel-${event.id}-${channel}`)
+    })
+
+    //
+    // Start translating for a channel
+    //
+    socket.on('start-channel', async ({ eventId = '', channel = '' }) => {
+      debug(`start-channel event=${eventId} channel=${channel}`)
+
+      // Make sure the user exists and they are a translator
+      const user = await getUser(socket.id, redis)
+      if (!user || !user.roles.includes('translator')) {
+        return sendError(socket, 'start-channel', 'bad_auth')
+      }
+
+      // Ensure the event exists
+      const event = events.find((e) => e.id === eventId)
+      if (!event) return sendError(socket, 'start-channel', `event.not_found`)
+
+      // Ensure the channel exists
+      const found = event.channels?.includes(channel)
+      if (!found) return sendError(socket, 'start-channel', `channel.not_found`)
+
+      // Generate the lock key
+      // This value points to the current translator for this channel
+      // (assuming 2 people broadcasting would be an issue)?
+      const key = `translator_${eventId}_${channel}`
+
+      // If there is already a translator for that room, remove them
+      const existing = await redis.get(key)
+      if (existing) {
+        await redis.del(`translator_${existing}`)
+        io.in(existing).emit('stop-channel-data')
+      }
+
+      // Make this new use the translator
+      // And store the socket-packet so they can send data
+      await redis.set(key, socket.id)
+      await redis.set(`translator_${socket.id}`, [eventId, channel].join(';'))
+    })
+
+    //
+    // Listen for translator packets and broadcast them
+    //
+    socket.on('channel-data', async (blob) => {
+      debug(`channel-data id=${socket.id}`)
+
+      const packet = await redis.get(`translator_${socket.id}`)
+      if (!packet) return sendError(socket, 'channel-data', 'bad_auth')
+
+      const [eventId, channel] = packet.split(';')
+
+      const key = `channel-${eventId}-${channel}`
+      debug('  sendTo=%s', key)
+      io.in(key).emit('channel-data', blob)
     })
 
     //
