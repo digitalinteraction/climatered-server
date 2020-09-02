1e6c06e3463b75fde7e96228faa80ca01cf95552
Rob Anderson <rob@andrsn.uk>
2020-09-02 11:51:54 +0100

    add unregister route
    - tests failing, upstream chowchow issue?

---
+++ src/routes/auth/__tests__/unregister.spec.ts
@@ -0,0 +1,27 @@
+import {
+  TypedMockChow,
+  createServer,
+  mocked,
+  AuthJwt,
+  createAuthToken,
+} from '../../../test-utils'
+import route from '../unregister'
+
+let chow: TypedMockChow
+let attendee: AuthJwt
+
+beforeEach(() => {
+  attendee = createAuthToken(['attendee'])
+  chow = createServer()
+  chow.apply(route)
+})
+
+describe('DELETE /me', () => {
+  it('should unregister that email', async () => {
+    mocked(chow.auth.fromRequest).mockResolvedValue(attendee)
+
+    await chow.http('delete', '/me')
+
+    expect(chow.users.unregister).toBeCalledWith(attendee.sub)
+  })
+})
