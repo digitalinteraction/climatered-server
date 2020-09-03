566b1751347fe00b66b5ba5dededd91c6d441869
Andy Garbett <atgarbett@gmail.com>
2020-09-03 14:04:42 +0100

    Added 'leave-lobby' socket event to leave lobby room (when user updates search preferences)

---
+++ src/sockets/coffee-chat/leave-lobby.ts
@@ -0,0 +1,44 @@
+import { TypedChow } from '../../server'
+import createDebug = require('debug')
+import { RedisService } from '../../services/redis'
+import { getLobbyKey, getLobbyUserSet } from './coffee-chat-utils'
+
+const debug = createDebug('api:socket:leave-lobby-options')
+
+async function checkForCurrentMatch(
+  redis: RedisService,
+  languagePrefs: string[],
+  topicPrefs: string[]
+) {
+  for (let lang of languagePrefs) {
+    for (let topic of topicPrefs) {
+      const matchedUser = await redis.setPop(getLobbyKey(lang, topic))
+      if (matchedUser) return matchedUser
+    }
+  }
+  return null
+}
+
+async function removeMatchedUser(redis: RedisService, socketId: string) {
+  const promises = []
+  const inLobby = await redis.setMembers(`inlobby-${socketId}`)
+  for (let lobby of inLobby) {
+    promises.push(redis.setRemove(lobby, socketId))
+    promises.push(redis.setRemove(getLobbyUserSet(socketId), lobby))
+  }
+  return Promise.all(promises)
+}
+
+export default function updateLobbyOptions(chow: TypedChow) {
+  chow.socket('leave-lobby', async (ctx, languagePrefs, topicPrefs) => {
+    const { socket, redis, emitToSocket } = ctx
+    debug(
+      `socket="${socket.id}" languagePrefs="${languagePrefs}" topicPrefs="${topicPrefs}"`
+    )
+    const match = await checkForCurrentMatch(redis, languagePrefs, topicPrefs)
+    if (match) {
+      await removeMatchedUser(redis, match)
+      socket.emitBack('left-lobby', true)
+    }
+  })
+}
