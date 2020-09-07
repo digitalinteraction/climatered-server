00da17b2212e4f002166428e699a5bcca8d45096
Rob Anderson <rob@andrsn.uk>
2020-09-07 09:56:34 +0100

    Add example test for join-room

---
+++ src/sockets/coffee-chat/__tests__/join-room.spec.ts
@@ -0,0 +1,37 @@
+import socket from '../join-room'
+import {
+  TypedMockChow,
+  createServer,
+  AuthJwt,
+  createAuthToken,
+  mocked,
+} from '../../../test-utils'
+
+let chow: TypedMockChow
+let attendee: AuthJwt
+
+beforeEach(() => {
+  chow = createServer()
+  attendee = createAuthToken(['attendee'])
+  socket(chow)
+})
+
+describe('@join-room', () => {
+  it('should join the room', async () => {
+    const socket = chow.io()
+
+    const user = {}
+    await socket.emit('join-room', 'some-room', user)
+
+    expect(socket.join).toBeCalledWith('some-room')
+  })
+
+  it('should emit the user to participants of that room', async () => {
+    const socket = chow.io()
+
+    const user = {}
+    await socket.emit('join-room', 'some-room', user)
+
+    expect(chow.emitToRoom).toBeCalledWith('some-room', 'user-joined', user)
+  })
+})
